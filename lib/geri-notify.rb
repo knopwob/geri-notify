49cf8ed073faeb474448ce57d05e23da4b4f3d91
Sascha Kruse <knopwob@googlemail.com>
2014-09-16 19:28:01 +0200

    fix up merge fragments

--- lib/geri-notify.rb
+++ lib/geri-notify.rb
@@ -1,5 +1,6 @@
 module GeriNotify
   require 'geri-notify/version'
+  require 'geri-notify/notification'
   require 'geri-notify/dbus-connector'
   require 'geri-notify/runner'
 end
---
+++ lib/geri-notify/notification.rb
@@ -0,0 +1,15 @@
+module GerriNotify
+  class Notification
+    def initialize(app_name, app_icon, summary, body, actions, hints, expire_time)
+      @app_name = app_name
+      @app_icon = app_icon
+      @summary = summary
+      @body = body
+      @actions = actions
+      @hints = hints
+      @expire_time = expire_time
+
+      puts "new notification: #{@app_name}: #{@summary} -- #{@body}"
+    end
+  end
+end
