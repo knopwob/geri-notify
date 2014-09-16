e84e7ddf3f32d8e036ddaf2384aa176d58ed918b
Sascha Kruse <knopwob@googlemail.com>
2014-09-16 22:48:56 +0200

    notification: urgency and expiration

--- lib/geri-notify/notification.rb
+++ lib/geri-notify/notification.rb
@@ -1,4 +1,6 @@
 module GeriNotify
+  require 'time'
+  require 'pry'
   class Notification
     def initialize(app_name, app_icon, summary, body, actions, hints, expire_time)
       @app_name = app_name
@@ -7,9 +9,39 @@ module GeriNotify
       @body = body
       @actions = actions
       @hints = hints
-      @expire_time = expire_time
+      if expire_time >= 0
+        # expire_time per api is in milliseconds but we use seconds internally
+        @expire_time = expire_time / 1000
+      else
+        @expire_time = 15 # TODO default time should be configurable
+      end
+
+      @timestamp = Time::now
+
+      if hints.has_key?("urgency")
+        case hints["urgency"]
+        when 0
+          @urgency = :low
+        when 1
+          @urgency = :normal
+        when 2
+          @urgency = :critical
+        else
+          @urgency = :normal
+        end
+      else
+        @urgency = :normal
+      end
 
       puts "new notification: #{@app_name}: #{@summary} -- #{@body}"
+    end
+
+    def expired?
+      if expire_time == 0
+          false
+      else
+          @timestamp + @expire_time > Time::now
+      end
     end
   end
 end
