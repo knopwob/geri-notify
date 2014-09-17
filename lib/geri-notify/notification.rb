d6c9b729286e1d611c9d0f93c3d3477a2566c7a7
Sascha Kruse <knopwob@googlemail.com>
2014-09-17 17:44:49 +0200

    Notification: implement Comparable

--- lib/geri-notify/notification.rb
+++ lib/geri-notify/notification.rb
@@ -2,6 +2,12 @@ module GeriNotify
   require 'time'
   require 'pry'
   class Notification
+
+    include Comparable
+
+    attr :urgency
+    attr :timestamp
+
     def initialize(app_name, app_icon, summary, body, actions, hints, expire_time)
       @app_name = app_name
       @app_icon = app_icon
@@ -43,6 +49,20 @@ module GeriNotify
         false
       else
         @timestamp + @expire_time > Time::now
+      end
+    end
+
+    def <=>(other)
+      if @urgency == other.urgency
+          other.timestamp <=> @timestamp
+      else
+        -1 if @urgency == :low
+        1  if @urgency == :critical
+        if other.urgency == :low
+          1
+        else
+          -1
+        end
       end
     end
   end
